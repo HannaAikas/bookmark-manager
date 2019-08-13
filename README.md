b73168dcbb44dea77a6f561cfceadbc4c9f07f3c
Hanna <hannaaikas@gmail.com>
2019-08-13 14:29:04 +0100

    Refactor: Tidy up Gemfile and app.rb dependencies

--- README.md
+++ README.md
@@ -7,6 +7,6 @@
 `I want to see list of bookmarks`
 
 
-### domain model
+### domain model:
 
 ![Domain_Model](./images/Bookmarks_Domain_Model.png)
--- app.rb
+++ app.rb
@@ -1,21 +1,18 @@
 require 'sinatra/base'
-require 'sinatra'
-require 'capybara/dsl'
-require 'selenium-webdriver'
-require 'bookmark'
+# require 'sinatra'
+# require 'capybara/dsl'
+# require 'selenium-webdriver'
+# require 'bookmark'
 
 class BookmarkManager < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :bookmarks
+  end
 
-get '/' do
-  erb :index
-end
-
-get '/bookmarks' do
-  @bookmarks = Bookmark.all
-  erb :bookmarks
-end
-
-run! if app_file == $0
-set :session_secret, 'super secret'
-
+  run! if app_file == $0
+  # set :session_secret, 'super secret'
 end
