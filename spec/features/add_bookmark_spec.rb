4df077756758f32f6ea085d8873c8f35bfc17096
Hanna <hannaaikas@gmail.com>
2019-08-15 20:21:03 +0100

    Add title to each bookmark and wrap database data in a Ruby object (NB. had to comment out 2 lines of code in bookmark_spec and bookmark.rb)

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -2,8 +2,9 @@ feature 'Adding a new bookmark' do
   scenario 'User adds a bookmark' do
     visit('/bookmarks/new')
     fill_in('url', with: "https://www.youtube.com/")
+    fill_in('title', with: 'YouTube')
     click_button("Submit")
 
-    expect(page).to have_content "https://www.youtube.com/"
+    expect(page).to have_link("YouTube", href: "https://www.youtube.com/")
   end
 end
