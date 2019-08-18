cc718c498fb565c4ea50fc7f63c3c60a6d1cb40c
Hanna <hannaaikas@gmail.com>
2019-08-18 10:05:16 +0100

    Add feature: User able to update bookmarks (step 13 of challenge)

---
+++ spec/features/update_bookmark_spec.rb
@@ -0,0 +1,19 @@
+feature 'update bookmark' do
+  scenario 'user creates bookmark and then updates it' do
+    bookmark = Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    visit('/bookmarks')
+    expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+
+    first('.bookmark').click_button 'Edit'
+    expect(current_path).to eq "/bookmarks/#{bookmark.id}/edit"
+
+    fill_in('url', with: "http://www.sneakersacademy.com")
+    fill_in('title', with: "Sneakers Academy")
+    click_button('Submit')
+
+    expect(current_path).to eq '/bookmarks'
+    expect(page).not_to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+    expect(page).to have_link('Sneakers Academy', href: 'http://www.sneakersacademy.com')
+
+  end
+end
